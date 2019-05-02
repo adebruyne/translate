f9ec5b1d41b34106491f2cd8d7b93da8793f6421
Aylin DeBruyne <adebruyne88@gmail.com>
2019-05-02 06:50:50 -0400

    creates first context object

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import LanguageContext from '../context/LanguageContext';
 
 export default class Button extends Component {
+    static contextType = LanguageContext;
     render() {
+        console.log(this.context)
         return (
+
             <button className="ui button primary">Submit</button>
         )
     }
