6c8d753aae7dfc3b6eac33edf33824ba0e96e9f8
Aylin DeBruyne <adebruyne88@gmail.com>
2019-05-02 07:01:35 -0400

    pull in context

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,10 +4,10 @@ import LanguageContext from '../context/LanguageContext';
 export default class Button extends Component {
     static contextType = LanguageContext;
     render() {
-        console.log(this.context)
+        const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
         return (
 
-            <button className="ui button primary">Submit</button>
+            <button className="ui button primary">{text}</button>
         )
     }
 }
