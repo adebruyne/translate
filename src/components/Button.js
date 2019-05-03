160131bf35f118b0bc2a4abf50a590fa14143fcf
Aylin DeBruyne <adebruyne88@gmail.com>
2019-05-03 06:47:43 -0400

    adds another context as an example

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
 import LanguageContext from '../context/LanguageContext';
-
+import ColorContext from '../context/ColorContext';
 export default class Button extends Component {
 
     render() {
 
         return (
+            <ColorContext.Consumer>
+                {(color) =>
+                    <button className={`ui button ${color}`}>
+                        <LanguageContext.Consumer>
+                            {(value) => value === 'english' ? 'Submit' : 'Voorleggen'}
+                        </LanguageContext.Consumer>
+                    </button>
 
-            <button className="ui button primary">
-                <LanguageContext.Consumer>
-                    {(value) => value === 'english' ? 'Submit' : 'Voorleggen'}
-                </LanguageContext.Consumer>
-            </button>
+                }
+
+            </ColorContext.Consumer>
         )
     }
 }
